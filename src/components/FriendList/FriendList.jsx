24f87506a1fec96e4e4147e15cf91c1e86ae977d
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-04-29 19:42:45 +0300

    add data and markup

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export function FriendList({friends}) {
+    return <ul className={css.friendList}>
+        {friends.map(({id, name, isOnline, avatar}) => {
+            return <li key={id} className={css.item}>
+                <span className={css.status}>{isOnline}</span>
+            <img className={css.avatar} src={avatar} alt={avatar + ' avatar'}  width="48" />
+                <p className={css.name}>{name}</p>
+            </li>
+        })}
+    </ul>
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.exact({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+    }))
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export function Statistics({ title, stats }) {
+    return <section className={css.statistics}>
+        <h2 className={css.title}>{title}</h2>
+
+        <ul className={css.statList}>
+            {stats.map(stat => <li key={stat.id} className={css.item}>
+                <span className={css.label}>{stat.label}</span>
+                <span className={css.percentage}>{stat.percentage}%</span>
+            </li>)}
+        </ul>
+</section>
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    
+    stats: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+    }))
+}
+
+
